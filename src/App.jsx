4ccec616d8672e2f5675abc44ba19ed1944e5d6d
Aditya Singh <adityasingh40675@gmail.com>
2024-06-18 03:48:28 +0530

    made basic html structure for todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useLayoutEffect } from 'react'
+import { SiTodoist } from "react-icons/si";
 
 function App() {
 
+  useLayoutEffect(() => {
+    document.body.style.backgroundColor = "#eeeeee";
+  });
+
   return (
-    <div>
-      <h1 className='text-3xl'>Hello</h1>
+    // whole container
+    <div className='flex flex-col min-h-screen'>
+      {/* This contains heading and add todo */}
+      <div className='flex flex-col w-full'>
+        <div className='flex justify-center'>
+          <SiTodoist className='my-16 h-12 w-8 mr-4 text-customBlue'/>
+          <h1 className='text-4xl underline text-customBlue my-16'>My Todo-s</h1>
+        </div>
+
+        <div className=''>
+
+        </div>
+      </div>
+
+      {/* All to-dos */}
+      <div>
+
+      </div>
     </div>
   )
 }
